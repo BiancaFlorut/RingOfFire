d460aaca84704106d2268612fe76ff6dabe9f460
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-05-23 13:32:49 +0200

    player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.sass'
 })
@@ -17,7 +18,7 @@ export class GameComponent {
   ngOnInit() {
     this.newGame();
     console.log(this.game);
-    
+
   }
 
   newGame() {
@@ -27,13 +28,11 @@ export class GameComponent {
   takeCard() {
     if (!this.pickCardAnimation) {
       this.currentCard = this.game?.stack.pop() || '';
-    console.log(this.currentCard);
-    
-    this.pickCardAnimation = true;
-
-    setTimeout(() => {
-      this.pickCardAnimation = false;
-    }, 1500);
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+        this.game?.playedCards.push(this.currentCard);
+      }, 1500);
     }
   }
 }
