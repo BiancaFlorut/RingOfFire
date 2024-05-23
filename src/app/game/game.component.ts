9e0cebfddad9afd66edb1331815c06c84004f643
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-05-23 12:47:28 +0200

    game structure

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -9,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './game.component.sass'
 })
 export class GameComponent {
+  pickCardAnimation = false;
+  game: Game | undefined;
 
+  ngOnInit() {
+    this.newGame();
+    console.log(this.game);
+    
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
 }
